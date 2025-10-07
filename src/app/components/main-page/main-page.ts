900a3ab1c10ecd675a45d92962b36585647cc103
Franco Angeletti <franco.angeletti01@gmail.com>
2025-10-07 17:05:43 -0300

    Agrego contenido de ejemplo en la tarjeta de notificación

--- src/app/components/main-page/main-page.ts
+++ src/app/components/main-page/main-page.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Notifications } from '../notifications/notifications';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [Notifications],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css'
 })
 export class MainPage {
+  showNotificationsPanel:boolean = false;
 
+  toggleNotificationsPanel():void {
+    this.showNotificationsPanel = !this.showNotificationsPanel;
+  }
 }
