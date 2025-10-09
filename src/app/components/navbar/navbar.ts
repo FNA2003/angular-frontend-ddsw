ff807d67893bd4654a4dd1cdc6936053c8209999
Franco Angeletti <franco.angeletti01@gmail.com>
2025-10-09 10:03:16 -0300

    Agrego animación a navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { LinkI } from '../../interfaces/navbar-link';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -11,8 +11,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './navbar.css'
 })
 export class NavbarComponent {
-  currentRoute:string = "";
-  navLinks:LinkI[] = [];
+  currentRoute:string = ""; // Ruta luego de redirecciones
+  navLinks:LinkI[] = []; // Links que se mostrará en el navbar (son dinámicos según la ruta)
+
+  navbarVisible:boolean = true; // Este booleano se usa para esconder el navbar según si se scrrollea hacia abajo
+  lastScrollTop:number = 0; // Cantidad de píxeles desplazados hacia arriba en el último scroll
+
 
   /* Links de las rutas sin acceso */
   public firstLinks:LinkI[] = [
@@ -42,5 +46,23 @@ export class NavbarComponent {
       this.currentRoute.startsWith(path)
     );    
     this.navLinks = isAuth ? this.firstLinks : this.appLinks;
+  }
+
+
+  
+
+  /* En este método detectamos si se scrollea hacia abajo o no
+    el decorator sirve para auto-agregar y remover el event listener del scroll*/
+  @HostListener("window:scroll", [])
+  onScroll = (): void => {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop; // Según el componente que se renderiza abajo, el scroll es de la ventana o de todo el html
+        
+    if (currentScroll > this.lastScrollTop && currentScroll > 100) {
+      this.navbarVisible = false;
+    } else {
+      this.navbarVisible = true;
+    }
+
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Si se scrolleo hacia abajo guardamos cuanto
   }
 }
