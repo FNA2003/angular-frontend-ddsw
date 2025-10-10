0f7914eb8d9a3c63ecd25d06433df3867aff7656
Franco Angeletti <franco.angeletti01@gmail.com>
2025-10-10 17:58:27 -0300

    Comienzo a separar los estilos de proyectos por tipo

--- src/app/components/projects/projects-list/projects-list.ts
+++ src/app/components/projects/projects-list/projects-list.ts
@@ -1,17 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Project, ProjectEnum } from '../../../models/project.model';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-projects-list',
-  imports: [DatePipe],
+  imports: [DatePipe, NgClass],
   templateUrl: './projects-list.html',
   styleUrl: './projects-list.css',
-  standalone:true
 })
 export class ProjectsList {
   @Input() projects:Project[] = [];
   @Input() projectType:ProjectEnum = ProjectEnum.PERSONAL;
+  prEnum = ProjectEnum;
 
   ngOnInit() {
     
