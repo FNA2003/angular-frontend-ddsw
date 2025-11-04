98d3008603a4ba53825f4aa0e41d5b3752ee5332
Ignacio <83927721+ignaciopieroni@users.noreply.github.com>
2025-11-04 14:32:53 -0300

    crear tareas y listarlas

--- src/app/components/tasks/list-personal-proyect-tasks/list-personal-proyect-tasks.ts
+++ src/app/components/tasks/list-personal-proyect-tasks/list-personal-proyect-tasks.ts
@@ -3,28 +3,84 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { ErrorParserService } from '../../../services/error-parser-service';
 import { ProjectsService } from '../../../services/projectsService';
+import { TasksService } from '../../../services/tasksService';
+import { Task } from '../../../models/task.model';
+import { EditTask } from "../task-editor-creator/task-editor-creator";
+
 
 @Component({
   selector: 'app-list-personal-proyect-tasks',
-  imports: [],
+  imports: [EditTask],
   templateUrl: './list-personal-proyect-tasks.html',
-  styleUrl: './list-personal-proyect-tasks.css'
+  styleUrl: './list-personal-proyect-tasks.css',
+  standalone:true
 })
 export class ListPersonalProyectTasks {
   project_id:number = -1;
+  tasks:Task[] = [];
+  editTaskId:number|null|undefined = null;
+
+  showEditor = false;              // controla el *ngIf del editor
+  editing: Task | null = null;     // tarea en edición (o null si es creación)
 
   constructor(private route: ActivatedRoute,
               private router:Router,
               private toastr:ToastrService,
               private errorParserService:ErrorParserService,
-              private projectsService:ProjectsService) {}
+              private projectsService:ProjectsService,
+              private tasksService: TasksService) {}
 
   ngOnInit() {
-    const project_id = this.route.snapshot.paramMap.get('project_id');
+    const project_id = Number(this.route.snapshot.paramMap.get('project_id'));
+
 
     if ((!project_id || isNaN(Number(project_id)))) {
       this.router.navigate(['/app'])
     }
     this.project_id = Number(project_id);
+
+    this.tasksService.getPersonalTasks(project_id).subscribe({
+      next:(t) => {
+        this.tasks = t;
+      },
+      error:(e) => {
+        this.toastr.error(this.errorParserService.parseBackendError(e), "Erro al tratar de obtener las tareas personales!");
+      }
+    });
   }
+
+  /* Atrapo el emmiter de 'editor-creator-task' para mostrar la tarea actualizada sin volver a consultar el back */
+  onSave(updatedTask:Task) {
+    let x;
+    for(x = 0; x < this.tasks.length; x++) {
+      if (this.tasks[x].id === updatedTask.id) { break; }
+    }
+    this.tasks[x] = updatedTask;
+    this.editTaskId = null;
+  }
+
+  deleteTask(task:Task) {
+      this.tasksService.removePersonalTask(this.project_id, task.id as number)
+        .subscribe({
+          next: (v:any) => {
+            this.toastr.success("Se eliminó correctamente la tarea", "Éxito al eliminar la tarea");
+            this.tasks.splice(this.tasks.indexOf(task), 1);
+          },
+          error: (e) => {
+            this.toastr.error(this.errorParserService.parseBackendError(e), "Error al tratar de eliminar la tarea");
+          }
+        });
+    }  
+  
+  closeTasks(){
+    for (const tarea of this.tasks){
+      this.deleteTask(tarea);
+    }
+  }
+
+  onCancelled() {
+  this.showEditor = false;
+  this.editing = null;
+}
+
 }
--- src/app/components/tasks/task-editor-creator/task-editor-creator.ts
+++ src/app/components/tasks/task-editor-creator/task-editor-creator.ts
@@ -1,11 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Project } from '../../../models/project.model';
+import { CommonModule, DatePipe, NgClass } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProjectsService } from '../../../services/projectsService';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UserDataService } from '../../../services/user-data-service';
+import { ErrorParserService } from '../../../services/error-parser-service';
+import { Task } from '../../../models/task.model';
+import { TasksService } from '../../../services/tasksService';
 
 @Component({
   selector: 'app-task-editor-creator',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './task-editor-creator.html',
   styleUrl: './task-editor-creator.css'
 })
-export class TaskEditorCreator {
+export class EditTask {
+  @Input() task:Task|null = null; //null : crea una tarea. not null: edita tarea
+  
+  @Output() save = new EventEmitter<Task>();
+  @Output() cancelled = new EventEmitter<void>();
 
+  formulario:FormGroup = new FormGroup({});
+
+  @Input() orgId?: number;
+  @Input() proyId?: number;
+
+  availableTasks: Task[] = [];
+
+  constructor (private toastr:ToastrService, 
+               private projectService:ProjectsService,
+               private userDataService:UserDataService,
+               private errorParserService:ErrorParserService,
+               private tasksService:TasksService) {  }
+
+  ngOnInit() {
+    this.formulario = new FormGroup({
+      // Los campos que se pueden editar
+      name:new FormControl(this.task?.name, [Validators.required, Validators.maxLength(32)]),
+      description:new FormControl(this.task?.description),
+      expiration_datetime:new FormControl(this.task?.expiration_datetime),
+
+      predecessors_ids:new FormControl(this.task?.predecessors_ids, [Validators.required, Validators.maxLength(32)]),
+      users_ids:new FormControl(this.task?.users_ids, [Validators.required, Validators.maxLength(32)]),
+      teams_ids:new FormControl(this.task?.teams_ids, [Validators.required, Validators.maxLength(32)]),
+
+      // Estos no :/
+      id:new FormControl(this.task?.id),
+      created_at:new FormControl(this.task?.created_at),
+      completed:new FormControl(this.task?.completed),
+      project:new FormControl(this.task?.project),
+
+      predecessors:new FormControl(this.task?.predecessors),
+      assigned_users:new FormControl(this.task?.assigned_users),
+      assigned_teams:new FormControl(this.task?.assigned_teams)  
+    });
+    this.tasksService.getPersonalTasks(this.proyId as number).subscribe(tasks => {
+      this.availableTasks = tasks;
+    });
+  }
+
+  // onSubmit() {
+  //   if (this.formulario.invalid) {
+  //     console.log(this.formulario.getRawValue());
+      
+  //     this.toastr.warning("Cerciorese de que todos los campos requeridos estén completos y en el formato pedido.", "Error en los campos!");
+  //     return;
+  //   }
+
+  //   const proyecto = this.formulario.getRawValue() as Project;
+  //   proyecto.created_at = new Date().toISOString().split('T')[0];
+
+  //   const dropdown:HTMLSelectElement = document.getElementById("projectTipeSelector") as HTMLSelectElement;
+
+  //   if (this.orgId? > 0) {
+  //     this.tasksService.makePersonalTask(this.orgId?, task)
+  //     .subscribe({
+  //       next:(val) => {
+  //         this.formulario.reset();
+  //         this.toastr.success("Proyecto creado correctamente", "Éxito al crear el proyecto");
+  //         this.router.navigate(["/app"]);
+  //       },
+  //       error:(e) => {
+  //         this.toastr.error(this.errorParserService.parseBackendError(e), "Error al crear el proyecto!");
+  //       }
+  //     });
+  //   } else {
+  //     this.projectsService.makePersonalProject(proyecto)
+  //     .subscribe({
+  //       next:(val) => {
+  //         this.formulario.reset();
+  //         this.toastr.success("Proyecto creado correctamente", "Éxito al crear el proyecto");
+  //         this.router.navigate(["/app"]);
+  //       },
+  //       error:(e) => {
+  //         this.toastr.error(this.errorParserService.parseBackendError(e), "Error al crear el proyecto!");
+  //       }
+  //     });
+  //   }
+    
+  // }
 }
